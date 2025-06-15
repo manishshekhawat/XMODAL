e5d0d1a009c77f10abd218b18839ac416fd79b8f
Manish <manishshekhawat756@gmail.com>
2025-06-15 14:40:05 +0530

    update1

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,23 +1,29 @@
-import { useState } from "react"
-import { Form } from "./Form"
-import  "./Modal.css"
+import { useState } from "react";
+import { Form } from "./Form";
+import "./Modal.css";
 
-export const Modal=()=>{
-    const [isButtonClicked,setIsbuttonClicked]=useState(false);
+export const Modal = () => {
+  const [isButtonClicked, setIsbuttonClicked] = useState(false);
 
-    const openForm=()=>{
-        setIsbuttonClicked(true);
-    }
+  const openForm = () => {
+    setIsbuttonClicked(true);
+  };
+  const closeForm = () => {
+    setIsbuttonClicked(false);
+  };
 
-    return(
-        <div className="container">
-            <h1>User Details Modal</h1>
-            <button onClick={openForm}>Open Form</button>
+  return (
+    <div className="container">
+      <h1>User Details Modal</h1>
+      <button onClick={openForm}>Open Form</button>
 
-            
-              {isButtonClicked && <Form setIsbuttonClicked={setIsbuttonClicked}/>}  
-            
-            
+      {isButtonClicked && (
+        <div className="modal" onClick={closeForm}>
+          <div className="modal-content" onClick={(e)=>e.stopPropagation()}>
+            <Form setIsbuttonClicked={setIsbuttonClicked} />
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
